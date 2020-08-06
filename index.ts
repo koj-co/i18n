9438584fcf589dba375c18f093ae67073f23cd63
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-06 20:39:32 +0530

    :heavy_minus_sign: Remove googleapis

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
-import { cosmic } from "@anandchowdhary/cosmic";
+import { google } from "googleapis";
+import { cosmicSync } from "@anandchowdhary/cosmic";
+
+const config = cosmicSync("i18n");
+
+const oauth2Client = new google.auth.OAuth2(
+  config.googleSheetsClientId,
+  config.googleSheetsClientSecret,
+  "https://developers.google.com/oauthplayground"
+);
+oauth2Client.setCredentials({
+  access_token: config.googleSheetsAccess,
+  refresh_token: config.googleSheetsRefresh,
+});
+
+const sheets = google.sheets("v4");
 
 export const i18n = async () => {
-  const config = await cosmic("i18n");
+  console.log("Starting i18n...");
+  const result = await sheets.spreadsheets.values.get({
+    spreadsheetId: config.sheetId,
+    range: "A1:A10",
+  });
+  console.log(result);
 };
 
 i18n();
