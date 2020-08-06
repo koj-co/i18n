49d765f25cbc382c23e18f01f2d7a7172bc67fb9
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-06 20:01:56 +0530

    :heavy_plus_sign: Add cosmic for config

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { config } from "dotenv";
 import { GoogleSpreadsheet } from "google-spreadsheet";
 config();
 
+const doc = new GoogleSpreadsheet("<the sheet ID from the url>");
+
 export const i18n = async () => {
   console.log("Starting i18n generator...");
 };
