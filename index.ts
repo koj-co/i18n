588d2f284242162600e31d711a9cc598e4cffdd3
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-06 20:53:07 +0530

    :sparkles: Add generator

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 import { cosmicSync } from "@anandchowdhary/cosmic";
 import axios from "axios";
+import { ensureDir, fstat, writeJson } from "fs-extra";
+import { join } from "path";
 
 const config = cosmicSync("i18n");
 const sheet = config.sheet || 1;
 
 export const i18n = async () => {
+  console.log("Starting...");
   const { data } = await axios.get(
     `https://spreadsheets.google.com/feeds/list/${config.sheetId}/${sheet}/public/values?alt=json`
   );
+  console.log("Fetched data");
   const rows: any[] = [];
   if (data && data.feed && data.feed.entry) {
     console.log(data.feed.entry);
@@ -28,13 +32,17 @@ export const i18n = async () => {
       rows.push(newRow);
     }
   }
+  await ensureDir(join(".", "locales"));
   const languages = Object.keys(rows[0]).filter(
     (key) => key.toLocaleLowerCase().trim() !== "key"
   );
   for await (const language of languages) {
-    //
+    const content: any = {};
+    rows.forEach((row) => (content[row.key] = row[language]));
+    await writeJson(join(".", "locales", `${language}.json`), content);
+    console.log("Created", language);
   }
-  console.log("rows", rows);
+  console.log("Done!");
 };
 
 i18n();
